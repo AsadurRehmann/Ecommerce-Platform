46e207000a81f0540f9732e2e06d690f51569df5
AsadurRehmann <asadurrehman886@gmail.com>
2025-09-09 03:44:23 +0500

    cooking

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          {/*user lauout*/}
+        </Route>
+        <Route>{/*Admin lauout*/}</Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
