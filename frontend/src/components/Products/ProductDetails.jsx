c61f18671f4bdf82699a7f40fe72e88af4fa8558
AsadurRehmann <asadurrehman886@gmail.com>
2025-09-13 04:57:15 +0500

    yeah

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 function ProductDetails() {
   const selectedProduct = {
@@ -38,7 +39,24 @@ function ProductDetails() {
 
   const handleQuantityChange = (action) => {
     if (action === "plus") setQuantity((prev) => prev + 1);
-    if (action === "minus" && quantity>1) setQuantity((prev) => prev - 1);
+    if (action === "minus" && quantity > 1) setQuantity((prev) => prev - 1);
+  };
+
+  const handleAddToCart = () => {
+    if (!selectedSize || !selectedColor) {
+      toast.error("Please select size and color before adding to cart.", {
+        duration: 1000,
+      });
+      return;
+    }
+    setIsButtonDisabled(true);
+
+    setTimeout(() => {
+      toast.success("Product added to cart!", {
+        duration: 1000,
+      });
+      setIsButtonDisabled(false);
+    }, 500);
   };
 
   return (
@@ -156,8 +174,16 @@ function ProductDetails() {
               </div>
             </div>
 
-            <button className="bg-black text-white py-2 px-6 rounded w-full mb-4">
-              ADD TO CART
+            <button
+              disabled={isButtonDisabled}
+              onClick={handleAddToCart}
+              className={`bg-black text-white py-2 px-6 rounded w-full mb-4 ${
+                isButtonDisabled
+                  ? "cursor-not-allowed opacity-50"
+                  : "hover:bg-gray-900"
+              }`}
+            >
+              {isButtonDisabled?"Adding....":"ADD TO CART"}
             </button>
 
             <div className="mt-10 text-gray-700">
