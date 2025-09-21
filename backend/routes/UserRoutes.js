a5445583e947814ba62b76a8a69aafcefb826237
AsadurRehmann <asadurrehman886@gmail.com>
2025-09-21 09:13:43 +0500

    login and register routes

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/Users.js");
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+
+        let user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: "user already exists" })
+
+        user = new User({ name, email, password });
+        await user.save();
+
+        //    jwt payload
+        const payload = { user: { id: user._id, role: user.role } };
+
+        // sign and return the user along with the token
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: "40h" },
+            (err, token) => {
+                if (err) throw err;
+
+                // send the user and token with response
+                res.status(201).json({
+                    user: {
+                        name: user.name
+                    }, token
+                })
+            }
+        )
+    } catch (err) {
+        console.log("ERROR", err)
+    }
+})
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user =await User.findOne({ email });
+
+        if (!user) return res.status(400).json({ message: "Invalid credentials" });
+        const isMatch=await user.matchPassword(password);
+        if(!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+         //    jwt payload
+        const payload = { user: { id: user._id, role: user.role } };
+
+        // sign and return the user along with the token
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: "40h" },
+            (err, token) => {
+                if (err) throw err;
+
+                // send the user and token with response
+                res.status(200).json({
+                    user: {
+                        name: user.name
+                    }, token
+                })
+            }
+        )
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("dayum")
+
+    }
+})
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv=require("dotenv");
-const connectDB=require("./config/db");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/UserRoutes.js");
 
-const app=express();
+const app = express();
 app.use(express.json());
 app.use(cors());
 dotenv.config();
-const PORT=process.env.PORT;
-connectDB(); 
+const PORT = process.env.APP_PORT;
+connectDB();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Holla");
 })
 
-app.listen(PORT,()=>{
+app.use("/api/users", userRoutes)
+
+app.listen(PORT, () => {
     console.log(`app is running at http://localhost:${PORT}`);
 })
