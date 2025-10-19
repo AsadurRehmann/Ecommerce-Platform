d881b11904b48a56972252b0589b64bca35c8474
AsadurRehmann <asadurrehman886@gmail.com>
2025-10-19 08:32:03 +0500

    plz deploy

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,18 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "https://shessshstylee.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
